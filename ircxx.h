a85a8fe24f75e7a45b1409ac545a16931fc7dce3
youngchief btw ツ <33383463+youngchief-btw@users.noreply.github.com>
2021-07-22 16:35:10 -0700

    Base code.

---
+++ ircxx.h
@@ -0,0 +1,16 @@
+#ifndef YOUNGCHIEF_IRCXX
+#define YOUNGCHIEF_IRCXX
+
+// Here's the start of the code
+// I did this as to not duplicate declarations when multiple things depend on this
+#ifdef __cplusplus /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+
+namespace ircxx {}
+
+#ifdef __cplusplus /* If this is a C++ compiler, end C linkage */
+}
+#endif
+
+#endif
